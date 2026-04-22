9704a134f2028609cb530b217f49088f8f3a94b2
ayush <ayushsonu821312@gmail.com>
2026-04-22 17:54:48 +0530

    making initRepo logic

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,13 @@ const { hideBin } = require('yargs/helpers');
 
 const { initRepo } = require("./controllers/init"); 
 const { addRepo } = require("./controllers/add"); 
+const { commitRepo } = require("./controllers/commit"); 
+const { pushRepo } = require("./controllers/push"); 
+const { pullRepo } = require("./controllers/pull"); 
+const { revertRepo } = require("./controllers/revert"); 
 
 yargs(hideBin(process.argv))
+
   .command(
     "init", 
     "Initialize the new repository", 
@@ -24,6 +29,45 @@ yargs(hideBin(process.argv))
     }, 
     addRepo
   )
+
+  .command(
+    "commit <message>", 
+    "commit the staged files", 
+    (yargs) => {yargs.positional("message" , {
+        describe : "commit message",
+        type : "string"
+    });
+    }, 
+    commitRepo
+  )
+
+  .command(
+    "push", 
+    "push commits to S3", 
+    {}, 
+    pushRepo
+  )
+
+  .command(
+    "pull", 
+    "push commits from S3", 
+    {}, 
+    pullRepo
+  )
+
+
+
+  .command(
+    "revert <commitID>", 
+    "Revert to a specific commit", 
+    (yargs) => {yargs.positional("commitID" , {
+        describe : "commit ID to revert to",
+        type : "string"
+    });
+    }, 
+    revertRepo
+  )
+
   .demandCommand(1, "You need at least one command")
   .help()
   .parse(); // Use .parse() instead of .argv for better reliability
