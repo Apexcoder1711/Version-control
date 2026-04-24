d2f0ca61d622713eac2cdf3b6c0c23c5ae8338f8
ayush <ayushsonu821312@gmail.com>
2026-04-24 18:30:56 +0530

    revert command done

--- backend/index.js
+++ backend/index.js
@@ -1,75 +1,60 @@
-const yargs = require('yargs/yargs');
-const { hideBin } = require('yargs/helpers');
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
 
-
-const { initRepo } = require("./controllers/init"); 
-const { addRepo } = require("./controllers/add"); 
-const { commitRepo } = require("./controllers/commit"); 
-const { pushRepo } = require("./controllers/push"); 
-const { pullRepo } = require("./controllers/pull"); 
-const { revertRepo } = require("./controllers/revert"); 
+const { initRepo } = require("./controllers/init");
+const { addRepo } = require("./controllers/add");
+const { commitRepo } = require("./controllers/commit");
+const { pushRepo } = require("./controllers/push");
+const { pullRepo } = require("./controllers/pull");
+const { revertRepo } = require("./controllers/revert");
 
 yargs(hideBin(process.argv))
+  .command("init", "Initialize the new repository", {}, initRepo)
 
   .command(
-    "init", 
-    "Initialize the new repository", 
-    {}, 
-    initRepo
-  )
-
-
-  .command(
-    "add <filePath>", 
-    "Add file to the repo", 
-    (yargs) => {yargs.positional("file" , {
-        describe : "File to add the staging area",
-        type : "string"
-    });
-    }, 
-    (argv) =>{
+    "add <filePath>",
+    "Add file to the repo",
+    (yargs) => {
+      yargs.positional("file", {
+        describe: "File to add the staging area",
+        type: "string",
+      });
+    },
+    (argv) => {
       addRepo(argv.filePath);
-    }
+    },
   )
 
   .command(
-    "commit <message>", 
-    "commit the staged files", 
-    (yargs) => {yargs.positional("message" , {
-        describe : "commit message",
-        type : "string"
-    });
-    }, 
-    (argv) =>{
+    "commit <message>",
+    "commit the staged files",
+    (yargs) => {
+      yargs.positional("message", {
+        describe: "commit message",
+        type: "string",
+      });
+    },
+    (argv) => {
       commitRepo(argv.message);
-    }
+    },
   )
 
-  .command(
-    "push", 
-    "push commits to S3", 
-    {}, 
-    pushRepo
-  )
+  .command("push", "push commits to S3", {}, pushRepo)
+
+  .command("pull", "push commits from S3", {}, pullRepo)
 
   .command(
-    "pull", 
-    "push commits from S3", 
-    {}, 
-    pullRepo
-  )
-
-
-
-  .command(
-    "revert <commitID>", 
-    "Revert to a specific commit", 
-    (yargs) => {yargs.positional("commitID" , {
-        describe : "commit ID to revert to",
-        type : "string"
-    });
-    }, 
-    revertRepo
+    "revert <commitID>",
+    "Revert to a specific commit",
+    (yargs) => {
+      yargs.positional("commitID", {
+        describe: "commit ID to revert to",
+        type: "string",
+      });
+    },
+    (argv) => {
+      revertRepo(argv.commitID);
+    },
   )
 
   .demandCommand(1, "You need at least one command")
